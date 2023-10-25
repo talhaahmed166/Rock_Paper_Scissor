f34cfc06a8c26282feab3bcfbe118d34133b1a5e
Talha Ahmed <144958338+talhaahmed166@users.noreply.github.com>
2023-10-25 06:02:48 -0400

    Add files via upload

---
+++ newGame.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string player1;
+    string player2;
+
+    cout << "Player 1 ";
+    cin >> player1;
+    cout << "Player 2 ";
+    cin >> player2;
+    if (player1 == "rock")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 1 WON!!! \x1b[0m";
+    }
+    if (player2 == "rock")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 2 WON!!! \x1b[0m";
+    }
+    if (player1 == "seasor" && player2 == "paper")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 1 WON!!! \x1b[0m";
+    }
+    if (player2 == "seasor" && player1 == "paper")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 2 WON!!! \x1b[0m";
+    }
+    if (player2 == "seasor" && player1 == "rock")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 1 WON!!! \x1b[0m";
+    }
+    if (player1 == "seasor" && player2 == "rock")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 2 WON!!! \x1b[0m";
+    }
+    if (player2 == "paper" && player1 == "rock")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 1 WON!!! \x1b[0m";
+    }
+    if (player1 == "paper" && player2 == "rock")
+    {
+        cout << "\x1b[42m\x1b[30m Congratulations... Player 2 WON!!! \x1b[0m";
+    }
+    return 0;
+}
